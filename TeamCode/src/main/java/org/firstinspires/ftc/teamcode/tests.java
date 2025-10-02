18f3062f9ddb5760b4babddeb1129d1a8d0d57e1
krish <joshikrish2022@gmail.com>
2025-10-02 11:37:21 -0500

    added shooter code

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/tests.java
@@ -0,0 +1,24 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+
+@TeleOp
+public class tests extends OpMode {
+
+    private DcMotor shooter = null ;
+
+    @Override
+    public void init() {
+        shooter = hardwareMap.get(DcMotor.class,"shooter");
+
+    }
+
+    @Override
+    public void loop(){
+        if(gamepad1.x) {
+            shooter.setPower(1);
+        }
+    }
+}
