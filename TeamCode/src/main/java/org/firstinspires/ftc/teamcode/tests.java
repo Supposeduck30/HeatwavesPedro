3fc741726ad264244bca0eaa5a3dca10d50219bb
praka <yashvi.prakash7@gmail.com>
2025-10-19 17:33:18 -0500

    forward movement test

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/tests.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/tests.java
@@ -10,10 +10,20 @@ public class tests extends OpMode {
     private DcMotor shooter1 = null ;
     private DcMotor shooter2 = null ;
 
+    private DcMotor leftFront = null;
+    private DcMotor rightFront = null;
+    private DcMotor rightBack = null;
+
+    private DcMotor leftBack = null;
+
     @Override
     public void init() {
         shooter1 = hardwareMap.get(DcMotor.class,"shooter1");
         shooter2 = hardwareMap.get(DcMotor.class, "shooter2");
+        leftFront = hardwareMap.get(DcMotor.class,"lf");
+        rightFront= hardwareMap.get(DcMotor.class, "rf");
+        rightBack = hardwareMap.get(DcMotor.class, "rr");
+        leftBack = hardwareMap.get(DcMotor.class, "rr");
     }
 
     @Override
@@ -24,6 +34,19 @@ public class tests extends OpMode {
         } else {
             shooter1.setPower(0);
             shooter2.setPower(0);
+        }
+
+        if (gamepad1.dpad_up) {
+            leftFront.setPower(0.5);
+            rightFront.setPower(0.5);
+            rightBack.setPower(0.5);
+            leftBack.setPower(0.5);
+        }
+        else {
+            leftFront.setPower(0);
+            rightFront.setPower(0);
+            rightBack.setPower(0);
+            leftBack.setPower(0);
         }
     }
 }
