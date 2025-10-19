dd8444f18e66b873ecdcfc6602daf4ab0a384ca8
krish <joshikrish2022@gmail.com>
2025-10-19 18:02:41 -0500

    bum bot code

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/tests.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/tests.java
@@ -2,6 +2,7 @@ package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.CRServo;
 import com.qualcomm.robotcore.hardware.DcMotor;
 
 @TeleOp
@@ -10,10 +11,13 @@ public class tests extends OpMode {
     private DcMotor shooter1 = null ;
     private DcMotor shooter2 = null ;
 
+    private CRServo intake = null;
+
     @Override
     public void init() {
         shooter1 = hardwareMap.get(DcMotor.class,"shooter1");
         shooter2 = hardwareMap.get(DcMotor.class, "shooter2");
+        intake = hardwareMap.get(CRServo.class, "intake");
     }
 
     @Override
@@ -24,6 +28,13 @@ public class tests extends OpMode {
         } else {
             shooter1.setPower(0);
             shooter2.setPower(0);
+        }
+
+        if (gamepad1.a){
+            intake.setPower(1);
+        }
+        else {
+            intake.setPower(0);
         }
     }
 }
